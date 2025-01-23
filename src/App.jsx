efc2cb9ad22630b32c4219da83325b231a5df698
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2025-01-23 11:27:57 +0545

    added className to control  form size

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="container">
       <header>Contact Form</header>
-      <Form />
+      {/* you can define larger-form and smaller-form based on the need */}
+      <div className="larger-form">
+        <Form />
+      </div>
     </div>
   );
 }
